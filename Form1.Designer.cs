20a41de8fc2d17a49b2afd33e6eec5b7c365a4e0
Maurizio Andres Baggio <maurizioandres.baggio@gmail.com>
2021-10-30 12:46:59 +0200

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace AudioSessionMonitorExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonSet1 = new System.Windows.Forms.Button();
+            this.buttonSet05 = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxDeviceName = new System.Windows.Forms.TextBox();
+            this.buttonInitialize = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(153, 75);
+            this.progressBar1.MarqueeAnimationSpeed = 1;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(448, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "MIC (Default Input Device)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 132);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(1095, 333);
+            this.textBox1.TabIndex = 2;
+            // 
+            // buttonSet1
+            // 
+            this.buttonSet1.Location = new System.Drawing.Point(501, 22);
+            this.buttonSet1.Name = "buttonSet1";
+            this.buttonSet1.Size = new System.Drawing.Size(122, 23);
+            this.buttonSet1.TabIndex = 3;
+            this.buttonSet1.Text = "set volume to 1.0";
+            this.buttonSet1.UseVisualStyleBackColor = true;
+            this.buttonSet1.Click += new System.EventHandler(this.buttonSet1_Click);
+            // 
+            // buttonSet05
+            // 
+            this.buttonSet05.Location = new System.Drawing.Point(373, 22);
+            this.buttonSet05.Name = "buttonSet05";
+            this.buttonSet05.Size = new System.Drawing.Size(122, 23);
+            this.buttonSet05.TabIndex = 3;
+            this.buttonSet05.Text = "set volume to 0.5";
+            this.buttonSet05.UseVisualStyleBackColor = true;
+            this.buttonSet05.Click += new System.EventHandler(this.buttonSet05_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(315, 22);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(52, 23);
+            this.buttonUpdate.TabIndex = 3;
+            this.buttonUpdate.Text = "update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(417, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "(of the latest session in the list)";
+            // 
+            // textBoxDeviceName
+            // 
+            this.textBoxDeviceName.Location = new System.Drawing.Point(93, 22);
+            this.textBoxDeviceName.Name = "textBoxDeviceName";
+            this.textBoxDeviceName.Size = new System.Drawing.Size(187, 20);
+            this.textBoxDeviceName.TabIndex = 5;
+            // 
+            // buttonInitialize
+            // 
+            this.buttonInitialize.Location = new System.Drawing.Point(12, 21);
+            this.buttonInitialize.Name = "buttonInitialize";
+            this.buttonInitialize.Size = new System.Drawing.Size(75, 23);
+            this.buttonInitialize.TabIndex = 6;
+            this.buttonInitialize.Text = "Initialize";
+            this.buttonInitialize.UseVisualStyleBackColor = true;
+            this.buttonInitialize.Click += new System.EventHandler(this.buttonInitialize_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(109, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Device Name";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 106);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(1095, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(638, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(469, 86);
+            this.panel1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1119, 477);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonInitialize);
+            this.Controls.Add(this.textBoxDeviceName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonSet05);
+            this.Controls.Add(this.buttonSet1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1135, 516);
+            this.MinimumSize = new System.Drawing.Size(1135, 516);
+            this.Name = "Form1";
+            this.Text = "Audio Session Monitor Example";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonSet1;
+        private System.Windows.Forms.Button buttonSet05;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxDeviceName;
+        private System.Windows.Forms.Button buttonInitialize;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,258 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+using System.Threading;
+using NAudio.CoreAudioApi;
+using System.Diagnostics;
+using NAudio.CoreAudioApi.Interfaces;
+using Timer = System.Timers.Timer;
+using System.Runtime.InteropServices;
+
+namespace AudioSessionMonitorExample
+{
+    public partial class Form1 : Form {
+        private static Random rnd = new Random();
+        private static double audioValueMax = 0;
+        private static double audioValueLast = 0;
+        private static int audioCount = 0;
+        private static int RATE = 48000;
+        private static int BUFFER_SAMPLES = 1024;
+        private static int timer_ms = 25;
+        private static Form1 form;
+
+        List<SoundDevice> soundDeviceList;
+
+        private SessionCollection sessions;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void AudioSessions() {
+            textBox1.Text = "";
+            soundDeviceList = new List<SoundDevice>();
+            var enumerator = new MMDeviceEnumerator();
+            foreach (var wasapi in enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.All)) {
+                try {
+                    if (wasapi.DeviceFriendlyName.Contains(textBoxDeviceName.Text) || wasapi.FriendlyName.Contains(textBoxDeviceName.Text)) {
+                        textBox2.Text = $"{wasapi.DataFlow} - {wasapi.FriendlyName} - {wasapi.DeviceFriendlyName} - {wasapi.State} - {wasapi.ID} - {wasapi.InstanceId}\r\n";
+                        sessions = wasapi.AudioSessionManager.Sessions;
+                        if (sessions == null) return;
+                        //var sessionList = new List<AudioSessionControl>(sessions.Count);
+                        for (int i = 0; i < sessions.Count; i++) {
+                            var session = sessions[i];
+                            if (session.IsSystemSoundsSession && ProcessExists(session.GetProcessID) && session.GetProcessID == 3860) {
+                                textBox1.Text += $"SystemSoundSession - {session.DisplayName}\r\n\t\t{session.GetSessionIdentifier}\r\n\t\tVolume relative to device: {session.SimpleAudioVolume.Volume}\r\n\t\tPeak Volume: {session.AudioMeterInformation.MasterPeakValue}\r\n\r\n";
+                                break;
+                            }
+                        }
+                        int j = 0;
+                        for (int i = 0; i < sessions.Count; i++) {
+                            var session = sessions[i];
+                            if (!session.IsSystemSoundsSession && ProcessExists(session.GetProcessID) && session.GetProcessID == 3860) {
+                                textBox1.Text += $"SoundSession - {session.DisplayName}\r\n\t\t{session.GetSessionIdentifier}\r\n\t\tVolume relative to device: {session.SimpleAudioVolume.Volume}\r\n\t\tPeak Volume: {session.AudioMeterInformation.MasterPeakValue}\r\n\r\n";
+                                soundDeviceList.Add(new SoundDevice(session, j));
+                                j++;
+                            }
+                        }
+                        break;
+                    }
+                }
+                catch (Exception ex) {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        bool ProcessExists(uint processId) {
+            try {
+                var process = Process.GetProcessById((int)processId);
+                return true;
+            }
+            catch (ArgumentException) {
+                return false;
+            }
+        }
+
+        private void InitializeMicrophone() {
+            var waveIn = new WaveInEvent();
+            waveIn.DeviceNumber = 0; // change this to select different sound inputs
+            waveIn.WaveFormat = new NAudio.Wave.WaveFormat(RATE, 1); // 1 for mono
+            waveIn.DataAvailable += OnDataAvailable; // this function must exist
+            waveIn.BufferMilliseconds = (int)((double)BUFFER_SAMPLES / (double)RATE * 1000.0);
+            waveIn.StartRecording();
+        }
+
+        private void OnDataAvailable(object sender, WaveInEventArgs args) {
+            float max = 0;
+
+            // interpret as 16 bit audio
+            for (int index = 0; index < args.BytesRecorded; index += 2) {
+                short sample = (short)((args.Buffer[index + 1] << 8) |
+                                        args.Buffer[index + 0]);
+                var sample32 = sample / 32768f; // to floating point
+                if (sample32 < 0) sample32 = -sample32; // absolute value 
+                if (sample32 > max) max = sample32; // is this the max value?
+            }
+
+            // calculate what fraction this peak is of previous peaks
+            if (max > audioValueMax) {
+                audioValueMax = (double)max;
+            }
+            audioValueLast = max;
+            audioCount += 1;
+            double frac = audioValueLast / audioValueMax;
+            try {
+                BeginInvoke(new Action(() => progressBar1.Value = (int)(max * 100)));
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            form = this;
+            InitializeMicrophone();
+            SendMessage(progressBar1.Handle,
+                0x400 + 16, //WM_USER + PBM_SETSTATE
+                0x0003, //PBST_PAUSED
+                0);
+
+            //SendMessage(progressBar1.Handle,
+            //      0x400 + 16, //WM_USER + PBM_SETSTATE
+            //      0x0002, //PBST_ERROR
+            //      0);
+        }
+
+        private void UpdateSessions() {
+            textBox1.Text = "";
+            if (soundDeviceList == null || soundDeviceList.Count == 0)
+                return;
+            foreach (SoundDevice sd in soundDeviceList) {
+                AudioSessionControl session = sd.GetSession;
+                textBox1.Text += $"SoundSession - {session.DisplayName}\r\n\t\t{session.GetSessionIdentifier}\r\n\t\tVolume relative to device: {session.SimpleAudioVolume.Volume}\r\n\t\tPeak Volume: {session.AudioMeterInformation.MasterPeakValue}\r\n\r\n";
+            }
+        }
+
+
+        private void buttonInitialize_Click(object sender, EventArgs e) {
+            if (!string.IsNullOrWhiteSpace(textBoxDeviceName.Text))
+                AudioSessions();
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e) {
+            UpdateSessions();
+        }
+
+        private void buttonSet05_Click(object sender, EventArgs e) {
+            if (soundDeviceList == null || soundDeviceList.Count == 0)
+                return;
+            soundDeviceList.Last().GetSession.SimpleAudioVolume.Volume = 0.5f;
+            UpdateSessions();
+        }
+
+        private void buttonSet1_Click(object sender, EventArgs e) {
+            if (soundDeviceList == null || soundDeviceList.Count == 0)
+                return;
+            soundDeviceList.Last().GetSession.SimpleAudioVolume.Volume = 1.0f;
+            UpdateSessions();
+        }
+
+        public class SoundDevice : IAudioSessionEventsHandler {
+            private AudioSessionControl _session;
+            public AudioSessionControl GetSession => _session;
+            private Timer _timer;
+            private ProgressBar _pbar;
+            private Label _label;
+            private int _offset;
+
+            public SoundDevice(AudioSessionControl sess, int offset) {
+                _session = sess;
+                _session.RegisterEventClient(this);
+                _offset = offset;
+                _pbar = new ProgressBar();
+                _pbar.Location = new Point(10 + (160 * _offset), 30);
+                _pbar.Size = new Size(_pbar.Width + 40, _pbar.Height);
+                _pbar.Maximum = 100;
+                _pbar.Minimum = 0;
+                _pbar.Visible = true;
+                _label = new Label();
+                _label.Location = new Point(10 + (160 * _offset), 10);
+                _label.Size = new Size(_label.Width + 40, _label.Height);
+                _label.Text = _session.DisplayName;
+                form.panel1.Controls.Add(_pbar);
+                form.panel1.Controls.Add(_label);
+                SendMessage(_pbar.Handle,
+                    0x400 + 16, //WM_USER + PBM_SETSTATE
+                    0x0003, //PBST_PAUSED
+                    0);
+                _timer = new System.Timers.Timer(timer_ms);
+                _timer.Elapsed += Timer_Elapsed;
+                _timer.AutoReset = false;
+                _timer.Start();
+            }
+
+            private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e) {
+                try {
+                    form.BeginInvoke(new Action(() => _pbar.Value = (int)(_session.AudioMeterInformation.MasterPeakValue * 100)));
+                }
+                catch (Exception ex) {
+                    Console.WriteLine(ex.Message);
+                }
+                _timer.Start(); // trigger next timer
+            }
+
+            public void OnChannelVolumeChanged(uint channelCount, IntPtr newVolumes, uint channelIndex) {
+                throw new NotImplementedException();
+            }
+
+            public void OnDisplayNameChanged(string displayName) {
+                throw new NotImplementedException();
+            }
+
+            public void OnGroupingParamChanged(ref Guid groupingId) {
+                throw new NotImplementedException();
+            }
+
+            public void OnIconPathChanged(string iconPath) {
+                throw new NotImplementedException();
+            }
+
+            public void OnSessionDisconnected(AudioSessionDisconnectReason disconnectReason) {
+                throw new NotImplementedException();
+            }
+
+            public void OnStateChanged(AudioSessionState state) {
+                throw new NotImplementedException();
+            }
+
+            public void OnVolumeChanged(float volume, bool isMuted) {
+                Console.WriteLine($"Vol changed to \"Listening device\" {_session.DisplayName}: " + volume.ToString());
+            }
+
+            public void Dispose() {
+                if (_session != null) {
+                    _session.UnRegisterEventClient(this);
+                    // I think Dispose calls UnRegisterEventClient anyway.. But belt and braces
+                    _session.Dispose();
+                    _session = null;   // null obj
+                    GC.Collect(); // Force GC collection
+                }
+            }
+        }
+        
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        static extern uint SendMessage(IntPtr hWnd,
+            uint Msg,
+            uint wParam,
+            uint lParam);
+    }
+}
